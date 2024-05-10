82a2fa746d7efab182041c3f02d860baf957f768
Aaron <61670051+MrBlu3Sky57@users.noreply.github.com>
2024-05-10 10:52:54 -0400

    Initial Commit with Restaurant Class

---
+++ Restaurant.java
@@ -0,0 +1,118 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Restaurant {
+    private String id;
+    private String name;
+    private String location;
+    private Map<String, String[]> menu;
+    private Map<String, String> tags;
+
+    /**
+     * Instantiate a restaurant object
+     * @param id Restaurant id
+     * @param name Restaurant name
+     * @param location Restaurant location
+     * @param menu Restaurant menu
+     * @param tags Restaurant tags
+     */
+    public Restaurant(String id, String name, String location, Map<String, String[]> menu, Map<String, String> tags) {
+        setId(id);
+        setName(name);
+        setLocation(location);
+        setMenu(menu);
+        setTags(tags);
+    }
+
+    /**
+     * Copy constructor for the restaurant class
+     * @param restaurant The restaurant object to be copied
+     */
+    public Restaurant(Restaurant restaurant) {
+        setId(restaurant.getId());
+        setName(restaurant.getName());
+        setLocation(restaurant.getLocation());
+        setMenu(restaurant.getMenu());
+        setTags(restaurant.getTags());
+    }
+
+    /**
+     * Gets the id of the restaurant
+     * @return The id of the restaurant
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Gets the name of the restaurant
+     * @return The name of the restaurant
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gets the location of the restaurant
+     * @return The location of the restaurant
+     */
+    public String getLocation() {
+        return location;
+    }
+    
+    /**
+     * Gets the menu of the restaurant
+     * @return A copy of the restaurant map
+     */
+    public Map<String, String[]> getMenu() {
+        return new HashMap<String, String[]>(menu);
+    }
+
+    /**
+     * The tags of the specifc restaurant
+     * @return The restaurant's tags
+     */
+    public Map<String, String> getTags() {
+        return new HashMap<String, String>(tags);
+    }
+
+    /**
+     * Set the id of the restaurant
+     * @param id The id of the restaurant
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * Set the name of the restaurant
+     * @param name The name of the restaurant
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Set the location of the restaurant
+     * @param location The location of the restaurant 
+     */
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    /**
+     * Set the menu of the restaurant
+     * @param menu The menu of the restaurant
+     */
+    public void setMenu(Map<String, String[]> menu) {
+        this.menu = menu;
+    }
+
+    /**
+     * Set the tags of a restaurant
+     * @param tags The tags for a restaurant
+     */
+    public void setTags(Map<String, String> tags) {
+        this.tags = new HashMap<String, String>(tags);
+    }
+}
