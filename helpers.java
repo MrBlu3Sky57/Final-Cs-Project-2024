83a71ef8c9c0a71b355df9db4c037c7fe504c595
Aaron <61670051+MrBlu3Sky57@users.noreply.github.com>
2024-05-15 18:54:09 -0400

    Add skeleton functions and clean up comments

--- helpers.java
+++ helpers.java
@@ -1,10 +1,32 @@
 import java.util.Map;
+import java.util.Set;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public interface helpers {
     final static String[] tagTypes = {"Cuisine", "Ambiance", "Price", "Style"};
     final static String[] ratingTypes = {"Taste", "Ambiance", "WorthIt", "Enjoy", "Hygiene", "Service"};
 
+    /**
+     * TO DO........
+     * @param db
+     * @param user_id
+     * @return
+     */
+    public static Map<String, Double> getUserRatings(String user_id) {
+        return null;
+    }
+
+
+    /**
+     * TO DO........
+     * @param db
+     * @param user_id
+     * @return
+     */
+    public static Map<String, Double> getRestrRatings(String restaurant_id) {
+        return null;
+    }
 
     /**
      * Gets the weight of each tag for a user to determine their preferences
@@ -39,6 +61,11 @@ public interface helpers {
         return weighted;
     }
 
+    /**
+     * Get the average rating for a restaurant for each tag value
+     * @param restrRatings Map of restaurant tag value and rating
+     * @return The average rating for a restaurant for each of its tag values
+     */
     public static Map<String, Double> getAvgRatings(Map<String, Double> restrRatings) {
         Map<String, Double> average = new HashMap<String, Double>(); // Map to store average rating per tag
         Map<String, Integer> amounts = new HashMap<String, Integer>(); // Map to store rating frequencies
@@ -65,6 +92,30 @@ public interface helpers {
         }
 
         return average;
+    }
+
+    public static Map<String, Restaurant> recommend(Map<String, Restaurant> restaurants, String user_id) {
+        Map<String, Double> userRatings = getUserRatings(user_id);
+        Map<String, Double> netRatings = new HashMap<String, Double>();
+
+        Set<String> keySet = restaurants.keySet();
+        String[] ids = new String[keySet.size()];
+        ids = keySet.toArray(ids);
+
+        for (String id : ids) {
+            Map<String, Double> restrRatings = getRestrRatings(id);
+
+            Double net = 0.0;
+            for (String key : restrRatings.keySet()) {
+                net += restrRatings.get(key) * userRatings.get(key);
+            }
+            
+            netRatings.put(id, net);
+
+            // Sort, Search then return list....
+        }
+
+        return null;
     }
 
 }
