7f51eb4ce26bf758fd599d355fc0606dce8b66b6
Aaron <61670051+MrBlu3Sky57@users.noreply.github.com>
2024-06-14 10:47:48 -0400

    Testing stuff

--- src/main/java/com/ufa/cs/project/Main.java
+++ src/main/java/com/ufa/cs/project/Main.java
@@ -1,8 +1,24 @@
 // Testing stuff
 package com.ufa.cs.project;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println(Helpers.authenticateUser("Aaron", "password"));
+        Map<String, Restaurant> restr = new HashMap<>();
+        Map<String, Double> menu = new HashMap<>();
+        Map<String, String> tags = new HashMap<>();
+
+        String name = "mcdonalds";
+        String location = "bayview";
+        menu.put("burger", 2.0);
+        tags.put("Cuisine", "American");
+        tags.put("Ambiance", "Tweaky");
+        tags.put("Price", "Cheap");
+        tags.put("Style", "Casual");
+
+        Helpers.addRestaurant(name, location, menu, tags, restr);
+        System.out.println(restr.get(Helpers.getRestrId(name)));
     }     
 }
